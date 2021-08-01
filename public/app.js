6d65db683dafb24176291847b49bcec61e4bdae4
sjworld45 <47642292+sjworld45@users.noreply.github.com>
2021-08-01 10:03:38 +0530

    second commit

--- public/app.js
+++ public/app.js
@@ -3,13 +3,15 @@ const signOutBtn = document.querySelector('.signOutBtn')
 const userDetails = document.querySelector('.user-details')
 const task_input = document.querySelector('.task_input')
 const submit_task_btn = document.querySelector('.submit_task_btn')
+const main = document.querySelector('main')
 const db = firebase.firestore()
 const task_list = document.querySelector('.tasks_list')
 let logged_in = false
 let user_id = ''
 let tasksRef;
 let unsubscribe;
-        tasksRef = db.collection('tasks')
+let serverTimestamp;
+tasksRef = db.collection('tasks')
 
 
 const signIn = () => {
@@ -17,13 +19,16 @@ const signIn = () => {
         signInBtn.classList.add('hidden')
         signOutBtn.classList.remove('hidden')
         signOutBtn.classList.add('visible')
-}
-
-const signOut = () => {
+        main.classList.add('visible')
+        
+    }
+    
+    const signOut = () => {
         signInBtn.classList.remove('hidden')
         signInBtn.classList.add('visible')
         signOutBtn.classList.remove('visible')
         signOutBtn.classList.add('hidden')
+        main.classList.add('hidden')
 }
 
 // authentication
@@ -32,14 +37,12 @@ const auth = firebase.auth()
 const provider = new firebase.auth.GoogleAuthProvider()
 
 signInBtn.onclick = () => {
-    console.log('click')
     auth.signInWithPopup(provider)
 }
 
 signOutBtn.onclick = () => auth.signOut();
 
 auth.onAuthStateChanged(user => {
-    console.log('state changed')
     if (user) {
         signIn()
         logged_in = true
@@ -47,12 +50,13 @@ auth.onAuthStateChanged(user => {
 
         const newUser = `<h3> Hello, ${user.displayName} </h3>`
         userDetails.innerHTML = newUser
-
+        serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
         unsubscribe = tasksRef
             .where('uid', '==', user.uid)
+            .orderBy("createdAt", "asc")
             .onSnapshot(querySnapshot => {
                 const items = querySnapshot.docs.map(doc => {
-                    return newTask(doc.data().name)
+                    return newTaskDiv(doc.data().name, doc.id, serverTimestamp())
                 })
                 task_list.innerHTML = items.join(' ')
             })
@@ -70,7 +74,9 @@ submit_task_btn.onclick = (e) => {
     e.preventDefault()
     const createNewTask = {
         uid: user_id,
-        name: task_input.value
+        name: task_input.value,
+        completed: false,
+        createdAt: serverTimestamp()
     }
     tasksRef.add(createNewTask)
     task_input.value = ''
@@ -79,6 +85,29 @@ submit_task_btn.onclick = (e) => {
 
 
 
-const newTask = (name) => {
-    return `<div class = "task shadow"> ${name} </div>`
+const newTaskDiv = (name, id) => {
+    return `<div class = "task shadow" id="${id}">
+                <p id=${id}-p>${name}</p>
+                <div class = "icons">
+                    <input id="${id}" type="checkbox">
+                    <i id="${id}" class="fa fa-trash" aria-hidden="true"></i>
+                </div>
+            </div>`
+}
+
+task_list.onclick =  (e) => {
+    const element = e.target
+    const id = element.id
+    console.log(element.type)
+    if (id){
+        if( element.type == "checkbox"){
+            // console.log(document.querySelector('.icons > input'))
+            document.querySelector('.icons > input').classList.add('completed')
+            db.batch().update(tasksRef.doc(id), {"completed":element.checked})
+        }
+        else {
+            tasksRef.doc(id).delete().then(console.log(`item with id ${id}, deleted`))
+        }
+    }
+    
 }
